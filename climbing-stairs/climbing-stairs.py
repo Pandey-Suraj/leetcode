410e4638b37a365dec84facf2368affb1d803de9
Suraj pandey <56708039+Pandey-Suraj@users.noreply.github.com>
2021-12-04 14:04:51 +0530

    Time: 32 ms (62.49%), Space: 14.3 MB (44.55%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+	def __init__(self):
+		self.hashMap = {1: 1, 2: 2}
+
+	def climbStairs(self, n):
+		if n not in self.hashMap:
+			self.hashMap[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+		return self.hashMap[n]
