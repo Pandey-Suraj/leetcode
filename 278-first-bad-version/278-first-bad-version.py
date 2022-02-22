c01d59bf952d335a9aae901b66cdeb4b6351e1b1
Suraj pandey <56708039+Pandey-Suraj@users.noreply.github.com>
2022-02-22 11:57:51 +0530

    Time: 39 ms (55.14%), Space: 13.9 MB (87.10%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -12,14 +12,10 @@ class Solution:
         l,r = 0 , n-1
         
         while(l<=r):
-            
             mid = (l+r)//2
-            
             if isBadVersion(mid) == False:
                 l = mid + 1
-            
             else:
-                r = mid - 1
-             
+                r = mid - 1   
         return l
         
